68c376c40268f9c2adb5edc03985257504d8dee7
Mingfei Huang <himax1023@gmail.com>
2017-12-06 09:07:16 -0500

    init starter

---
+++ src/controllers/api.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+import * as async from 'async';
+import { NextFunction, Request, Response } from 'express';
+import * as graph from 'fbgraph';
+import * as request from 'request';
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+export let getApi = (req: Request, res: Response) => {
+  res.render('api/index', {
+    title: 'API Examples',
+  });
+};
+
+/**
+ * GET /api/facebook
+ * Facebook API example.
+ */
+export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.user.tokens.find((token: any) => token.kind === 'facebook');
+  graph.setAccessToken(token.accessToken);
+  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+    if (err) { return next(err); }
+    res.render('api/facebook', {
+      title: 'Facebook API',
+      profile: results,
+    });
+  });
+};
